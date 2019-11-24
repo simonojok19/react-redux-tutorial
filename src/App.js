89bb82e051f510ee5527b0c5fe077a05ce24c7e5
Simon Peter Ojok <simonojok19@gmail.com>
2019-11-24 14:45:16 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import StudentList from './StudentList.jsx';
+const studentList = [
+  {id:1,name:'John Doe',grade:1,school:'React Redux School'},
+  {id:2,name:'Jane Doe',grade:2,school:'React Redux School'},  
+  {id:3,name:'Terry Adams',grade:3,school:'React Redux School'},
+  {id:4,name:'Jenny Smith',grade:4,school:'React Redux School'}
+];
+if (localStorage.getItem("student") === null) {
+  localStorage.setItem('student', JSON.stringify(studentList));
 }
-
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      studentList: []
+    }
+    this.editStudentSubmit = this.editStudentSubmit.bind(this);
+    this.deleteStudent = this.deleteStudent.bind(this);
+    this.addNewStudent = this.addNewStudent.bind(this);
+  }
+  componentWillMount() {
+    let studentList = JSON.parse(localStorage.getItem("student"));
+    this.setState((prevState, props) => ({
+      studentList: studentList
+    }));
+  }
+  addNewStudent() {
+    this.setState((prevState, props) => ({
+      studentList: [...prevState.studentList, {  
+        id: Math.max(...prevState.studentList.map(function(o){
+          return o.id
+        })) + 1,name: '', grade: 1, school: '' 
+      }]
+    }));
+  }
+  deleteStudent(id) {
+    let r = window.confirm("Do you want to delete this item");
+    if (r === true) {
+      let filteredStudentList = this.state.studentList.filter(
+        x => x.id !== id
+      );
+       this.setState((prevState, props) => ({
+          studentList: filteredStudentList
+       }));
+       localStorage.setItem(
+         'student',
+         JSON.stringify(filteredStudentList)
+       );
+    }
+  }
+  editStudentSubmit(id,name, grade, school) {
+    let studentListCopy = this.state.studentList.map((student) => {
+    
+      if (student.id === id) {
+        student.name = name;
+        student.grade = grade;
+        student.school = school;
+      }
+      return student;
+    });
+    this.setState((prevState, props) => ({
+      studentList: studentListCopy
+    }));
+    localStorage.setItem(
+      'student',
+      JSON.stringify(studentListCopy)
+    );
+  }
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row mt-3"><div className="col-lg-12">
+          <div className="card">
+            <div className="card-header">
+              Student Registry
+            </div>
+            <div className="card-body">
+              <table className="table table-hover">
+                <thead className="thead-dark"><tr><th>Name</th><th>Grade</th><th>School</th><th>Edit/Save</th><th>Delete</th></tr></thead>
+                <StudentList
+                  deleteStudent={this.deleteStudent}
+                  studentList={this.state.studentList}
+                  editStudentSubmit={this.editStudentSubmit}
+                />
+              </table>
+            <button
+              className="btn btn-dark pull-left"
+              onClick={this.addNewStudent}>
+              Add New
+            </button>
+          </div>
+        </div>
+      </div>
+     </div>
+    </div>
+   );
+  }
+}
 export default App;
